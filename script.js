dee59fd176aefeb5cac94778d491d051a7242f34
fowlerbr <brennan.fowler@mail.utoronto.ca>
2018-05-31 17:07:34 -0400

    added highlighting for non-static cells on hover and click

---
+++ script.js
@@ -0,0 +1,30 @@
+const cells = document.querySelectorAll("#soduko-box td");
+var selected = null;
+
+function highlight(current) {
+  if (current != selected) {
+    current.style.backgroundColor = "#D4EFDF";
+  }
+}
+
+function unhighlight(current) {
+  if (current != selected) {
+    current.style.backgroundColor = "white";
+  }
+}
+
+function select(current) {
+  if (selected != null) {
+    selected.style.backgroundColor = "white";
+  }
+  selected = current;
+  selected.style.backgroundColor = "#A9DFBF";
+}
+
+for (var i = 0; i < cells.length; i++) {
+  if (!cells[i].classList.contains("static")) {
+    cells[i].addEventListener('mouseover', function() { highlight(this) });
+    cells[i].addEventListener('mouseout', function() { unhighlight(this) });
+    cells[i].addEventListener('click', function() { select(this)})
+  }
+}
