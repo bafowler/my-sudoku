747a6f43127c45bb294fbeb1aa2d000868788383
fowlerbr <brennan.fowler@mail.utoronto.ca>
2018-06-06 12:50:55 -0400

    fixed tabbing

--- script.js
+++ script.js
@@ -2,41 +2,41 @@ const cells = document.querySelectorAll("#soduko-box td");
 var selected = null;
 
 function validateInput(current, event) {
-  if (!current.hasAttribute("readonly")) {
-    var char = String.fromCharCode(event.keyCode);
-    if(isNaN(char) || char == '0') {
-      return false;
+    if (!current.hasAttribute("readonly")) {
+        var char = String.fromCharCode(event.keyCode);
+        if(isNaN(char) || char == '0') {
+            return false;
+        }
+        current.value = char;
+        return true;
     }
-    current.value = char;
-    return true;
-  }
-  return false;
+    return false;
 }
 
 function highlight(current) {
-  if (current != selected) {
-    current.style.backgroundColor = "#D4EFDF";
-  }
+    if (current != selected) {
+        current.style.backgroundColor = "#D4EFDF";
+    }
 }
 
 function unhighlight(current) {
-  if (current != selected) {
-    current.style.backgroundColor = "white";
-  }
+    if (current != selected) {
+        current.style.backgroundColor = "white";
+    }
 }
 
 function select(current) {
-  if (selected != null) {
-    selected.style.backgroundColor = "white";
-  }
-  selected = current;
-  selected.style.backgroundColor = "#A9DFBF";
+    if (selected != null) {
+        selected.style.backgroundColor = "white";
+    }
+    selected = current;
+    selected.style.backgroundColor = "#A9DFBF";
 }
 
 for (var i = 0; i < cells.length; i++) {
-  if (!cells[i].classList.contains("static")) {
-    cells[i].addEventListener('mouseover', function() { highlight(this) });
-    cells[i].addEventListener('mouseout', function() { unhighlight(this) });
-    cells[i].addEventListener('click', function() { select(this)})
-  }
+    if (!cells[i].classList.contains("static")) {
+        cells[i].addEventListener('mouseover', function() { highlight(this) });
+        cells[i].addEventListener('mouseout', function() { unhighlight(this) });
+        cells[i].addEventListener('click', function() { select(this)})
+    }
 }
