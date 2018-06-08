767a7fdd451aebdf3bc1050102e81540d5dcc0c8
fowlerbr <brennan.fowler@mail.utoronto.ca>
2018-06-08 14:02:17 -0400

    added comments to every function in script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// every cell in the soduko game, including static AND non-static
 const cells = document.querySelectorAll("#soduko-box td");
+// the cell currently selected (clicked) by the user
 var selected = null;
 
+// switch between single/multiple mode for the selected cell's input
+// using shift key
 document.body.onkeydown = function (e) {
     if (e.keyCode == 16) {
         if (selected) {
@@ -9,12 +13,15 @@ document.body.onkeydown = function (e) {
     }
 }
 
+// ensure correct input is focused when its container cell is clicked
 document.body.onclick = function (e) {
     if (selected) {
         selected.querySelector("input").focus();
     }
 }
 
+// validate the input of a user into cell current
+// only allow numbers from 1-9
 function validateInput(current, event) {
     if (!current.hasAttribute("readonly")) {
         var char = String.fromCharCode(event.keyCode);
@@ -27,6 +34,12 @@ function validateInput(current, event) {
     return false;
 }
 
+// change the value of the input tag inside cell current to char, or add char
+// to the already existing value if the cell is in 'multiple' mode
+// if cell input is in 'single' mode ('multiple' class is absent), allow only
+// one digit input
+// if cell input is in 'multiple' mode ('multiple' class is present), allow
+// one to four digit input
 function changeCellValue(current, char) {
     if (current.value.length == 1 && !current.classList.contains("multiple")) {
         current.value = char;
@@ -36,6 +49,8 @@ function changeCellValue(current, char) {
     }
 }
 
+// toggle cell input between 'multiple' and 'single' (marked by the absence
+// of multiple)
 function changeCellState(current, event) {
     current = current.querySelector("input");
     if (current.classList.contains("multiple")) {
@@ -50,18 +65,22 @@ function changeCellState(current, event) {
     }
 }
 
+// shade cell current a light green
 function highlight(current) {
     if (current != selected) {
         current.style.backgroundColor = "#D4EFDF";
     }
 }
 
+// shade cell current white
 function unhighlight(current) {
     if (current != selected) {
         current.style.backgroundColor = "white";
     }
 }
 
+// shade cell current a darker green (only one cell should be this color at
+// a time)
 function select(current) {
     if (selected != null) {
         selected.style.backgroundColor = "white";
@@ -70,6 +89,7 @@ function select(current) {
     selected.style.backgroundColor = "#A9DFBF";
 }
 
+// on user confirm, clear the soduko board of all user input
 function clearBoard() {
     if (confirm("Are you sure you'd like to clear the board?")) {
         for (var i = 0; i < cells.length; i++) {
@@ -83,6 +103,7 @@ function clearBoard() {
     }
 }
 
+// add behaviours to all non-static cells
 for (var i = 0; i < cells.length; i++) {
     if (!cells[i].classList.contains("static")) {
         cells[i].addEventListener('mouseover', function() { highlight(this) });
